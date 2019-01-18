7334d4f68349705a91403fb253626aa1cf5d7eba
Mo Weiss <mshwss@gmail.com>
2019-01-18 11:12:36 -0500

    Start and Exit

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -6,7 +6,21 @@ namespace VirtualPetsAmok
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Pet Shelter!");
+            Console.WriteLine("You will choose a pet later.");
+            Console.WriteLine("Press 0 to Exit");
+            string userInput = Console.ReadLine();
+
+            if (userInput.Equals("0"))
+            {
+                Console.WriteLine("Good-bye. Come again soon.");
+                Environment.Exit(0);
+                
+            }
+
+             Console.WriteLine("Patience Please!");
+            
         }
+
     }
 }
