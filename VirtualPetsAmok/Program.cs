51b3ced4f70b24f077ee671956f992e224d38d9d
Mo Weiss <mshwss@gmail.com>
2019-01-22 10:59:14 -0500

    updated menu

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -2,24 +2,32 @@
 
 namespace VirtualPetsAmok
 {
-    class Program
-    {
-        static void Main(string[] args)
+    
+    
+        class Program
         {
-            Console.WriteLine("Welcome to the Pet Shelter!");
-            Console.WriteLine("You will choose a pet later.");
-            Console.WriteLine("Press 0 to Exit");
-            string userInput = Console.ReadLine();
-
-            if (userInput.Equals("0"))
+            static void Main(string[] args)
             {
-                Console.WriteLine("Good-bye. Come again soon.");
-                Environment.Exit(0);
+                Console.WriteLine("Welcome to the Pet Shelter!");
+                Console.WriteLine("Type a word to continue: \n\n\t" +
+
+                    "E - Exit \n\t" +
+                    "P - View Pet \n\t" +
+                    "I - View Instructions");
+                Console.WriteLine("");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower().Equals("e"))
+                {
+                    Console.WriteLine("Good-bye. Come again soon.");
+                    Environment.Exit(0);
+                }
+
+
+                Console.WriteLine("Patience Please!");
+
             }
 
-             Console.WriteLine("Patience Please!");
-            
         }
-
-    }
 }
+
