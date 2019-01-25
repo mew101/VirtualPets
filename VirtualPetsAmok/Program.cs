7c39d52983b8615bf190e7b30c018ff2acddb3d5
Robert <Robert.scott190@gmail.com>
2019-01-25 11:03:35 -0500

    Added status bars & Window size

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -6,6 +6,7 @@ namespace VirtualPetsAmok
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(124, 40);
             Console.BackgroundColor = ConsoleColor.DarkBlue; //Change Console background color 
 
             //VirtualPet[] pet1 = new VirtualPet[2];
@@ -44,9 +45,12 @@ namespace VirtualPetsAmok
                         Console.WriteLine("\n\tYour pet is a " + pet1.Species + ".");
                         Console.WriteLine("\n\tYour pet's name is: " + pet1.Name);
                         Console.WriteLine("\n\tYour pet's age is: " + pet1.Age);
-                        Console.WriteLine("\n\tYour pet's fullness level is: " + pet1.Fullness);
-                        Console.WriteLine("\n\tYour pet's happiness level is: " + pet1.Happiness);
-                        Console.WriteLine("\n\tYour pet's energy level is: " + pet1.Energy);
+                        Console.Write("\n\tYour pet's fullness level is: ");
+                        PrintStatusBar(pet1.Fullness,2);
+                        Console.Write("\n\tYour pet's happiness level is: "); 
+                        PrintStatusBar(pet1.Happiness,2);
+                        Console.Write("\n\tYour pet's energy level is: ");
+                        PrintStatusBar(pet1.Energy,2);
                         Console.WriteLine("_____________________________________________");
                         Console.WriteLine("Choose an action from the menu");
                         Console.WriteLine("F - Feed");
@@ -116,5 +120,47 @@ namespace VirtualPetsAmok
             
         }
 
+        public static void PrintStatusBar(int howMuch, int spaceMult)
+        {
+            //ConsoleColor currentBackground = Console.BackgroundColor;
+            //ConsoleColor currentForeground = Console.ForegroundColor;
+
+            //max is the number which will show 100% full bar
+            int max = 10;
+            //spaceMult is how many "blocks" per 1 unit, this is just for readability & aesthetics
+            //int spaceMult = 2;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue; //make font color easier to read inside bar
+            //3 Color Scale: Green, Yellow, Red
+
+            if (howMuch > ((2.0 / 3.0) * (double)max))//If number is between MAX and 2/3 of MAX
+            {
+                //Console.WriteLine("1st if green");
+                Console.BackgroundColor = ConsoleColor.Green;
+                for (int i = 1; i <= howMuch * spaceMult; i++)
+                    Console.Write(" ");
+            }
+            else if (howMuch > ((1.0 / 3.0) * (double)max))//If number is < 2/3 and > 1/3 of MAX
+            {
+                //Console.WriteLine("2nd if yellow");
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                for (int i = 1; i <= howMuch * spaceMult; i++)
+                    Console.Write(" ");
+            }
+            else //If number is less than 1/3 of MAX
+            {
+                //Console.WriteLine("3rd if red");
+                Console.BackgroundColor = ConsoleColor.Red;
+                for (int i = 1; i <= howMuch * spaceMult; i++)
+                    Console.Write(" ");
+            }
+            if (howMuch > 99)
+                Console.Write("\b\b");// Add 2 extra backspaces in case number is 3 digits
+            else if (howMuch > 9)
+                Console.Write("\b");// Add 1 extra backspace in case number is 2 digits
+
+            Console.Write("\b" + howMuch + "\n");
+            Console.ResetColor();
+        }
     }
 }
